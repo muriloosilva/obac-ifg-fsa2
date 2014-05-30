401d3d2a75e6f23517f931ddc6d0cc09c7faf512
matheus.wisni@gmail.com <matheus.wisni@gmail.com@095db15f-677c-0e41-b5c9-00efc19637a4>
2014-05-30 13:38:58 +0000

    Modelos do Objeto, Superficie e Ambiente "semi-finalizados". O modelo da escala, por ser pouco mais complexo, será finalizado posteriormente.

--- trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/modelo/ModeloAmbiente.java
+++ trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/modelo/ModeloAmbiente.java
@@ -1,9 +1,42 @@
 package br.edu.ifg.formosa.obac.modelo;
 
 public class ModeloAmbiente {
-	//Variáveis
-	//Double
-	public static double coefAtrito; //Recebe ou o atrito da madeira, ou do asfalto ou do aluminio
+	//Constantes
+	//--Double
+	public static final double gravidadeTerra = 9.8;
+	public static final double gravidadeLua = 1.6;
+	public static final double gravidadeMarte = 3.7;
 	
-	private double tempo;	
+	//Variáveis
+	//--Double
+	public static double coefAtrito; //Recebe ou o atrito da madeira, ou do asfalto ou do aluminio
+	public static double gravSelecionada; // Recebe ou a gravidade da Terra, da Lua ou de Marte
+	
+	private double tempo; //OBAC1
+	
+	//--Objeto, Escala e Superficie
+	private ModeloEscala mE = new ModeloEscala();
+	private ModeloObjeto mO = new ModeloObjeto();
+	private ModeloSuperficie mS = new ModeloSuperficie();
+	
+	//Metodos
+	//--Getters
+	public static double getGravidadeterra() {return gravidadeTerra;}
+	public static double getGravidadelua() {return gravidadeLua;}
+	public static double getGravidademarte() {return gravidadeMarte;}
+	public static double getCoefAtrito() {return coefAtrito;}
+	public static double getGravSelecionada() {return gravSelecionada;}
+	public double getTempo() {return tempo;}
+	public ModeloEscala getmE() {return mE;}
+	public ModeloObjeto getmO() {return mO;}
+	public ModeloSuperficie getmS() {return mS;}
+	
+	//--Setters
+	public static void setCoefAtrito(double coefAtrito) {ModeloAmbiente.coefAtrito = coefAtrito;}
+	public static void setGravSelecionada(double gravSelecionada) {ModeloAmbiente.gravSelecionada = gravSelecionada;}
+	public void setTempo(double tempo) {this.tempo = tempo;}
+	public void setmE(ModeloEscala mE) {this.mE = mE;}
+	public void setmO(ModeloObjeto mO) {this.mO = mO;}
+	public void setmS(ModeloSuperficie mS) {this.mS = mS;}
+	
 }
