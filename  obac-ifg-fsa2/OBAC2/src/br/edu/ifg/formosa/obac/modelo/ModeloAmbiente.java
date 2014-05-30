5620afe95bec71f884ae893944bb495065e0508f
matheus.wisni@gmail.com <matheus.wisni@gmail.com@095db15f-677c-0e41-b5c9-00efc19637a4>
2014-05-30 13:20:33 +0000

    Modelo do Objeto pronto mas nao finalizado

---
+++  obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/modelo/ModeloAmbiente.java
@@ -0,0 +1,9 @@
+package br.edu.ifg.formosa.obac.modelo;
+
+public class ModeloAmbiente {
+	//Variáveis
+	//Double
+	public static double coefAtrito; //Recebe ou o atrito da madeira, ou do asfalto ou do aluminio
+	
+	private double tempo;	
+}
