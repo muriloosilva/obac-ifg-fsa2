aacf5afe62bb35dc2cc72b5bc36faa1a82775490
matheus.wisni@gmail.com <matheus.wisni@gmail.com@095db15f-677c-0e41-b5c9-00efc19637a4>
2014-06-09 00:44:33 +0000

    Esboço da sobreposição dos painéis.

--- trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/visao/VisaoPainelSimulacao.java
+++ trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/visao/VisaoPainelSimulacao.java
@@ -1,5 +1,38 @@
 package br.edu.ifg.formosa.obac.visao;
 
-public class VisaoPainelSimulacao {
+import javax.swing.JPanel;
 
+public class VisaoPainelSimulacao extends JPanel {
+	//Constantes
+	//--Long
+	private static final long serialVersionUID = 1L;
+	
+	//Variaveis
+	//--Superficie
+	private VisaoSuperficie vS = null;
+	//--Objeto
+	private VisaoObjeto vO = null;
+	//--Propulsao
+	private VisaoPropulsao vP = null;
+	//--Escala
+	private VisaoEscala vE = null;
+	
+	//Metodos
+	//--Construtor
+	public VisaoPainelSimulacao() {
+		super(null);
+		
+		this.setSize(750, 600);
+		this.setLocation(250, 0);
+		
+		vS = new VisaoSuperficie();
+		vO = new VisaoObjeto();
+		vP = new VisaoPropulsao();
+		vE = new VisaoEscala();
+		
+		this.add(vE);
+		this.add(vP);
+		this.add(vO);
+		this.add(vS);
+	}
 }
--- trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/visao/VisaoSuperficie.java
+++ trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/visao/VisaoSuperficie.java
@@ -1,5 +1,32 @@
 package br.edu.ifg.formosa.obac.visao;
 
-public class VisaoSuperficie {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
+import javax.swing.JPanel;
+
+public class VisaoSuperficie extends JPanel {
+	//Constantes
+	//--Long
+	private static final long serialVersionUID = 1L;
+
+	//Metodos
+	//--Construtor
+	public VisaoSuperficie() {
+		super(null);
+		
+		this.setSize(750, 600);
+	}
+	
+	//--Paint
+	public void paint (Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g2d.setColor(Color.white);
+		g2d.fillRect(80, 80, 300, 200);
+		
+		g2d.setColor(Color.black);
+		g2d.drawString("Este retangulo pertence a superficie", 80, 90);
+	}
 }
