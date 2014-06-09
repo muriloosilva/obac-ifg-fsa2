6f8ea495f1ccdb478f15dde2c90933de23243d04
matheus.wisni@gmail.com <matheus.wisni@gmail.com@095db15f-677c-0e41-b5c9-00efc19637a4>
2014-06-09 17:06:25 +0000

    Escala v1.0.1 -Inicialmente pensado uma escala p cada tipo de simulação 	-Escala vai se ajustar automaticamente p cada tipo de simulação -Possivel desenhar 'n' marcadores 	-Tem um bug com quantidades grandes de marcadores (31+) -So funciona com 99% de eficiencia na horizontal

---  obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/controle/escala/ControleEscala.java
+++  obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/controle/escala/ControleEscala.java
@@ -6,7 +6,13 @@ public class ControleEscala {
 		
 	}
 	
-	public static int retornaMetade(int inicio, int fim) {
-		return inicio + ((fim - inicio) / 2);
+	//--Metodo utilizado para deteminar espacamento dos marcadores na escala
+	public static int retornaPedaco(int inicio, int fim, int marcadores) {
+		return (fim - inicio) / (marcadores + 1);
+		/*
+		 * Explicacao 'marcadores + 1'
+		 * Se o numero de marcadores for 1, a linha (escala) tera de ser dividida em duas partes (1 mar + 1 = 2 partes)
+		 * Se o numero de marcadores for 8, a linha (escala) tera de ser dividida em nove partes (8 mar + 1 = 9 partes)
+		 */
 	}
 }
