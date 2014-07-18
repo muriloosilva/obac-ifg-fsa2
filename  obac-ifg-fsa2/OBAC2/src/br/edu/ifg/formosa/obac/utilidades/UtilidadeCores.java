c083c4963b3ee6512cc988017b8a4578bafafc56
drakos.nevescunha8@gmail.com <drakos.nevescunha8@gmail.com@095db15f-677c-0e41-b5c9-00efc19637a4>
2014-07-18 00:15:29 +0000

    Novas cores criadas para o painel

---  obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/utilidades/UtilidadeCores.java
+++  obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/utilidades/UtilidadeCores.java
@@ -11,7 +11,8 @@ public class UtilidadeCores {
 	public static Color azul = new Color(0, 0, 255);
 	public static Color azulCobalto = new Color(0,71,171);
 	public static Color azulMetalico = new Color(70,130,180);
-	public static Color azulRoyal = new Color(65,105,225);
+	public static Color azulMedio = new Color(50,50,205);
+	public static Color azulNeon = new Color(7, 77, 255);
 	public static Color azulCeu = new Color(135,206,235);
 	public static Color azulClaro = new Color(173,216,230);
 	
@@ -27,6 +28,7 @@ public class UtilidadeCores {
 	public static Color amareloQueimado = new Color(238, 173, 45);
 	public static Color amareloAcafrao = new Color(244, 196, 48);
 	public static Color amarelo = new Color(255, 255, 0);
+	public static Color amareloLimao = new Color(255,247,0);
 	public static Color amareloClaro = new Color(255, 255, 224);
 	public static Color amareloKhaki = new Color(240, 230, 140);
 	
