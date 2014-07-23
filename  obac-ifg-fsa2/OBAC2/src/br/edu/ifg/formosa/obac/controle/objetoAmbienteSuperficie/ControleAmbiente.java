84bed9641e6532e74c601811cb81e5952108c363
drakos.nevescunha8@gmail.com <drakos.nevescunha8@gmail.com@095db15f-677c-0e41-b5c9-00efc19637a4>
2014-07-23 12:52:10 +0000

    Adicionada a função do bloqueio de alguns componentes do painel de configuração de acordo com a simulação

---  obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/controle/objetoAmbienteSuperficie/ControleAmbiente.java
+++  obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/controle/objetoAmbienteSuperficie/ControleAmbiente.java
@@ -11,9 +11,15 @@ import br.edu.ifg.formosa.obac.visao.VisaoPainelConfiguracao;
 import br.edu.ifg.formosa.obac.visao.VisaoSuperficie;
 
 public class ControleAmbiente {
+	
+	//Constante do painel de configuração
+	private final VisaoPainelConfiguracao vPC;
+	
 	//Métodos
 	//--Construtor
 	public ControleAmbiente(final ModeloAmbiente mA, final VisaoPainelConfiguracao vPC, final ModeloPainelConfiguracao mPC, final ControleOBAC cO, final VisaoSuperficie vS) {
+		this.vPC = vPC;
+		
 		vPC.getCsAmbienteGravidade().addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
@@ -37,18 +43,23 @@ public class ControleAmbiente {
 			public void actionPerformed(ActionEvent e) {
 				switch (vPC.getCsAmbienteSimulacao().getSelectedIndex()) {
 					case 0: //Plano
+						componentesUsados(true, true);
 						mA.setUrlA("plano");
 						break;
 					case 1: //Subida
+						componentesUsados(true, true);
 						mA.setUrlA("subida");
 						break;
 					case 2: //Descida
+						componentesUsados(true, true);
 						mA.setUrlA("descida");
 						break;
 					case 3: //P&P
+						componentesUsados(false, true);
 						mA.setUrlA("precipicio");
 						break;
 					case 4: //Queda
+						componentesUsados(false, false);
 						mA.setUrlA("plano");
 						
 						if (!vPC.getBaNovaSimulacao().isVisible()) 
@@ -58,6 +69,7 @@ public class ControleAmbiente {
 						
 						break;
 					case 5: //Projétil
+						componentesUsados(false, true);
 						mA.setUrlA("plano");
 						break;
 				}
@@ -71,4 +83,21 @@ public class ControleAmbiente {
 		vS.novasImagens(mA.getUrlGr(), mA.getUrlA(), mA.getUrlGu());
 		cO.repinta();
 	}
+	
+	//--Ativa ou desativa o componentes do pConfig de acordo com a simulação
+	private void componentesUsados(boolean obstaculo, boolean propulsao){
+		//Obstáculo
+			vPC.getBoColisaoNao().setSelected(true);
+			vPC.getBoColisaoNao().setEnabled(obstaculo);
+			vPC.getBoColisaoSim().setEnabled(obstaculo);
+		//Propulsão
+			vPC.getCsPropulsao().setEnabled(propulsao);
+			vPC.getCtPropulsaoDado1().setEnabled(propulsao);
+			vPC.getCtPropulsaoDado2().setEnabled(propulsao);
+			if(!propulsao){
+				vPC.getCtPropulsaoDado1().setText("");
+				vPC.getCtPropulsaoDado2().setText("");
+			}
+	}
+	
 }
