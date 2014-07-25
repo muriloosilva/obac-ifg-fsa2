14e213ff1d088a225a92d6f07400d91711df99d1
drakos.nevescunha8@gmail.com <drakos.nevescunha8@gmail.com@095db15f-677c-0e41-b5c9-00efc19637a4>
2014-07-25 14:41:55 +0000

    Variáveis renomeadas e criadas adição/aleração dos getters e setters

--- trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/modelo/ModeloObjeto.java
+++ trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/modelo/ModeloObjeto.java
@@ -53,13 +53,13 @@ public class ModeloObjeto {
 	private double posicaoXMetros = 0;	//Posicao em Metros
 	private double posicaoYMetros = 0;
 	
-	private double posicaoXPx; //Posicao em Pixels
-	private double posicaoYPx;
+	private int posicaoXPx; //Posicao em Pixels
+	private int posicaoYPx;
 	
 	private double posFinalXMetros = 0;//Posição final do objeto em metros no eixo X
-	private double posFinalXPix = 0;//Posição final do objeto em Pixels no eixo X
+	private int posFinalXPix = 0;//Posição final do objeto em Pixels no eixo X
 	private double posFinalYMetros = 0;//Posição final do objeto em metros no eixo Y
-	private double posFinalYPix = 0;//Posição final do objeto em Pixels no eixo Y 
+	private int posFinalYPix = 0;//Posição final do objeto em Pixels no eixo Y 
 	
 	private double forcaNormal = 0;//Força normal agindo no objeto
 	
@@ -74,27 +74,36 @@ public class ModeloObjeto {
 	public double getAceleracaoY() {return aceleracaoY;}
 	public double getPosicaoXMetros() {return posicaoXMetros;}
 	public double getPosicaoYMetros() {return posicaoYMetros;}
-	public double getPosicaoXPx() {return posicaoXPx;}
-	public double getPosicaoYPx() {return posicaoYPx;}
+	public int getPosicaoXPx() {return posicaoXPx;}
+	public int getPosicaoYPx() {return posicaoYPx;}
 	public double getForcaNormal(){return forcaNormal;}
 	public double getPosFinalXMetros(){return posFinalXMetros;}
-	public double getPosFinalXPix(){return posFinalXPix;}
+	public int getPosFinalXPix(){return posFinalXPix;}
 	public double getPosFinalYMetros(){return posFinalYMetros;}
-	public double getPosFinalYPix(){return posFinalYPix;}
+	public int getPosFinalYPix(){return posFinalYPix;}
 	
 	//Setters
 	public void setMassa(double massa) {this.massa = massa;}
 	public void setCoefRestituicao(double coefRestituicao) {this.coefRestituicao = coefRestituicao;}
 	public void setAceleracaoY(double aceleracaoY) {this.aceleracaoY = aceleracaoY;}
-	public void setPosicaoXPx(double posicaoXPx) {this.posicaoXPx = posicaoXPx;}
-	public void setPosicaoYPx(double posicaoYPx) {this.posicaoYPx = posicaoYPx;}
-	public void setPosFinalXPix(double posFinalXPix){this.posFinalXPix = posFinalXPix;}
-	public void setPosFinalYPix(double posFinalYPix){this.posFinalYPix = posFinalYPix;}
+	public void setPosicaoXPx(int posicaoXPx) {this.posicaoXPx = posicaoXPx;}
+	public void setPosicaoYPx(int posicaoYPx) {this.posicaoYPx = posicaoYPx;}
+	public void setPosFinalXPix(int posFinalXPix){this.posFinalXPix = posFinalXPix;}
+	public void setPosFinalYPix(int posFinalYPix){this.posFinalYPix = posFinalYPix;}
 	
 	//Setters relacionados ao Painel de Informação
-	public void setPosFinalYMetros(double posFinalYMetros){this.posFinalYMetros = posFinalYMetros;}
-	public void setVelocidade(double velocidade) {this.velocidade = velocidade;}
-	public void setVelocidadeInicial(double velocidadeInicial) {this.velocidadeInicial = velocidadeInicial;}
+	public void setPosFinalYMetros(double posFinalYMetros){
+		this.posFinalYMetros = posFinalYMetros;
+		cpi.mudaValorPosFinalY(posFinalYMetros);
+	}
+	public void setVelocidade(double velocidade) {
+		this.velocidade = velocidade;
+		cpi.mudaValorVelocidadePosColisao(velocidade);
+	}
+	public void setVelocidadeInicial(double velocidadeInicial) {
+		this.velocidadeInicial = velocidadeInicial;
+		cpi.mudaValorVelocidade(velocidadeInicial);
+	}
 	public void setAceleracao(double aceleracao) {
 		this.aceleracao = aceleracao;
 		cpi.mudaValorAceleracao(this.aceleracao);//Altera o valor da aceleração no PInfo
@@ -113,7 +122,7 @@ public class ModeloObjeto {
 	}
 	public void setPosFinalXMetros(double posFinalXMetros){
 		this.posFinalXMetros = posFinalXMetros;
-		cpi.mudaValorPosFinal(this.posFinalXMetros);
+		cpi.mudaValorPosFinalX(this.posFinalXMetros);
 	}
 	
 }
