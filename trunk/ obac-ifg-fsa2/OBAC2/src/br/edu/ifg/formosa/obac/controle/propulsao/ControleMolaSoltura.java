f79cce551df4db10d1bfef92d5a8c4a554961468
drakos.nevescunha8@gmail.com <drakos.nevescunha8@gmail.com@095db15f-677c-0e41-b5c9-00efc19637a4>
2014-07-25 14:26:36 +0000

    ControleMolaImagens foi substituida por ControleMolaSoltura, assim quando a mola é solte uma thread e ativada para mover a mola até a posição padrão

---
+++ trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/controle/propulsao/ControleMolaSoltura.java
@@ -0,0 +1,80 @@
+package br.edu.ifg.formosa.obac.controle.propulsao;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+import br.edu.ifg.formosa.obac.controle.obac.ControleOBAC;
+import br.edu.ifg.formosa.obac.modelo.ModeloAmbiente;
+import br.edu.ifg.formosa.obac.visao.VisaoObjeto;
+import br.edu.ifg.formosa.obac.visao.VisaoPropulsao;
+
+public class ControleMolaSoltura implements Runnable{
+	
+	private VisaoObjeto vo = null;
+	private VisaoPropulsao vp = null;
+	private ModeloAmbiente ma = null;
+	private ControleOBAC cOBAC = null;
+	private Thread t = null;
+	
+	public ControleMolaSoltura(VisaoObjeto vo, VisaoPropulsao vp, ModeloAmbiente ma, ControleOBAC cOBAC, ControleMolaMouse cmm) {
+		this.vo = vo;
+		this.vp = vp;
+		this.ma = ma;
+		this.cOBAC = cOBAC;
+		
+		//Remoção dos mouse*listeners
+		cmm.desativaMolaMouse();
+		
+		t = new Thread(this);
+		t.start();
+	}
+	
+	@SuppressWarnings({ "deprecation", "static-access" })
+	@Override
+	public void run() {
+		//É calculada a taxa de deformação e a velocidade de lançamento do objeto
+		ma.getmM().calculaX();
+//		ma.getmM().velocidadeLancamento();
+		//###Retornar com a velocidade assim que o controle principal da simulação estiver funcionando
+		
+		//Variáveis usadas no retorno da mola
+			double cont = 0.5;
+			int atraso = 20;
+		//Laço de repetição que traz a mola para a posição inicial
+		for (double i = ma.getmM().getTamanhoMolaAtualPix();
+			i < ma.getmM().getTamanhoMolaTotalPix();
+			i+=cont)
+		{
+			//Move o objeto
+			ma.getmO().setPosicaoXPx((int)i);
+			//Altera o tamanho da imagem para ajustála a compressão
+			vp.setImagemPropulsao(new ImageIcon(ma.getmM().getImagemMola().getImage().getScaledInstance((int)i, 30, Image.SCALE_DEFAULT)));
+			//Corrige o valor na variável do tamanho da mola localizado no modelo
+			ma.getmM().setTamanhoMolaAtualPix((int)i);
+			//Repaint
+			cOBAC.repinta();
+			//Sleep - pausas no carregamento
+			try {t.sleep(atraso);}
+			catch (InterruptedException e) {e.printStackTrace();}
+			//Incremento do segundo contador para dá a impressão de aceleração da mola
+			cont+=0.5;
+		}
+		
+		//Correção dos valores do tamnho da mola e da posição do objeto para evitar o desconforto visual
+		//Ocorre apenas quando a imagem não está no tamanho correto
+		if(ma.getmM().getTamanhoMolaAtualPix()<ma.getmM().getTamanhoMolaTotalPix()){
+			//Retorna a mola para a posição original (primeiro no modelo depois na imagem)
+			ma.getmM().setTamanhoMolaAtualPix(ma.getmM().getTamanhoMolaTotalPix());
+			vp.setImagemPropulsao(ma.getmM().getImagemMola());
+			//Posiciona o Objeto na posição original
+			ma.getmO().setPosicaoXPx(130);
+			//Repaint
+			cOBAC.repinta();
+		}
+			
+		//Para esta Thrad
+		t.stop();
+	}
+	
+}
