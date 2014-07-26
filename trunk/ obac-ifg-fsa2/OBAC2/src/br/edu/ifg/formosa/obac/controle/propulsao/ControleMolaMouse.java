f0fdbcb5b2ec22327b7dc274a8b5bd6496c45762
drakos.nevescunha8@gmail.com <drakos.nevescunha8@gmail.com@095db15f-677c-0e41-b5c9-00efc19637a4>
2014-07-26 13:15:05 +0000

    Troca de números per variáveis

--- trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/controle/propulsao/ControleMolaMouse.java
+++ trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/controle/propulsao/ControleMolaMouse.java
@@ -55,17 +55,21 @@ public class ControleMolaMouse {
 	
 	//Método utilizado para reposicionar o objeto e comprimir a mola
 	//Também indica quando ocorre a soltura do objeto, dando inicio a simulaçao 
+	@SuppressWarnings("static-access")
 	private void reposicionaObjeto(int x, int y){
 		//Booleana de controle - Verifica se a posição do mouse está em uma área válida
-		areaDaMola =
-			((x>=55 && x<=130)//Confirma se a posição em que o mouse está é permitida no eixo x(30px do epaço vazio e mais 20px para a mola não desaparecer)
-			 &&(y>=470 && y<=500));//Confirma se a posição do mouse no eixo Y está correta
+		areaDaMola =(
+			//Confirma se a posição em que o mouse está é permitida no eixo x(30px do epaço vazio e mais 20px para a mola não desaparecer)
+			(x>=(vp.getPosIniXProp()+ma.getmM().getTamanhoMolaMinimoPix())&& x<=(vp.getPosIniXProp()+ma.getmM().getTamanhoMolaTotalPix()))
+			 //Confirma se a posição do mouse no eixo Y está correta
+			&&(y>=vp.getPosIniYProp() && y<=(vp.getPosIniYProp()+ma.getmO().altura))
+		);
 			  
 		if(areaDaMola){
 			//Move o objeto
 			ma.getmO().setPosicaoXPx(x);
 			//Passa o Tamanho da mola de pixel para metros
-			ma.getmM().setTamanhoMolaAtualPix(x-30);
+			ma.getmM().setTamanhoMolaAtualPix(x-vp.getPosIniXProp());
 			ma.getmM().setTamanhoMolaTotalM((UtilidadeConvercoesEscala.convertePixelMetro(ma.getmM().getTamanhoMolaTotalM(), ma.getmM().getTamanhoMolaAtualPix(), ma.getmM().getTamanhoMolaTotalPix())));
 			//Altera o tamanho da imagem para ajustála a compressão
 			vp.setImagemPropulsao(new ImageIcon(ma.getmM().getImagemMola().getImage().getScaledInstance(ma.getmM().getTamanhoMolaAtualPix(), 30, Image.SCALE_DEFAULT)));
