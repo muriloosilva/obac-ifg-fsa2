f683800f5360532771f8b2aec26cb54ea728d737
drakos.nevescunha8@gmail.com <drakos.nevescunha8@gmail.com@095db15f-677c-0e41-b5c9-00efc19637a4>
2014-09-22 18:29:01 +0000

    Controles dos listener dos sliders da mola

---
+++ trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/controle/propulsao/ControleMolaSolturaDeslizante.java
@@ -0,0 +1,83 @@
+package br.edu.ifg.formosa.obac.controle.propulsao;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+import br.edu.ifg.formosa.obac.controle.obac.ControleOBAC;
+import br.edu.ifg.formosa.obac.controle.objetoAmbienteSuperficie.ControleInicioSimulacoes;
+import br.edu.ifg.formosa.obac.modelo.ModeloAmbiente;
+import br.edu.ifg.formosa.obac.visao.VisaoPropulsao;
+
+public class ControleMolaSolturaDeslizante implements Runnable{
+
+	private VisaoPropulsao vp = null;
+	private ModeloAmbiente ma = null;
+	private ControleOBAC cOBAC = null;
+//	private ControleMolaMouse cMM = null;
+	private ControleInicioSimulacoes cIS =null;
+	private Thread t = null;
+	
+	//Variável para o reposicionamento do objeto
+	private int reposicionaObjeto = 0;
+	
+	public ControleMolaSolturaDeslizante(VisaoPropulsao vp,ModeloAmbiente ma,
+										 ControleOBAC cOBAC, ControleInicioSimulacoes cIS)
+	{
+		this.vp = vp;
+		this.ma = ma;
+		this.cOBAC = cOBAC;
+		this.cIS = cIS;
+		
+		//É calculada a velocidade de lançamento do objeto
+		ma.getmP().getModeloMola().calculaVelocidadeLancamento();
+		
+		t = new Thread(this);
+		t.start();
+	}
+	
+	@SuppressWarnings({ "deprecation", "static-access" })
+	@Override
+	public void run() {
+		//Variáveis usadas no retorno da mola
+			double cont = 0.5;
+			int atraso = 20;
+		//Laço de repetição que traz a mola para a posição inicial
+		for (double i = ma.getmP().getModeloMola().getTamanhoMolaAtualPix();
+			i < ma.getmP().getModeloMola().getTamanhoMolaTotalPix();
+			i+=cont)
+		{
+			//Altera o tamanho da imagem para ajustála a compressão
+			vp.setImagemPropulsao(new ImageIcon(ma.getmP().getImagemPropulsao().getImage().getScaledInstance((int)i, 30, Image.SCALE_DEFAULT)));
+			//Corrige o valor na variável do tamanho da mola localizado no modelo
+			ma.getmP().getModeloMola().setTamanhoMolaAtualPix((int)i);
+			//Move o objeto (valor de i+posição inicial do objeto+ 1 pixel para o objeto ficar na frente da mola)
+			ma.getmO().setPosicaoXPx((int)i+ma.getmP().getPosXM()+1);
+			//Repaint
+			cOBAC.repinta();
+			//Sleep - pausas no carregamento
+			try {t.sleep(atraso);}
+			catch (InterruptedException e) {e.printStackTrace();}
+			//Incremento do segundo contador para dá a impressão de aceleração da mola
+			cont+=0.5;
+		}
+		
+		//Correção dos valores do tamnho da mola e da posição do objeto para evitar o desconforto visual
+		//Ocorre apenas quando a imagem não está no tamanho correto
+		if(ma.getmP().getModeloMola().getTamanhoMolaAtualPix()<ma.getmP().getModeloMola().getTamanhoMolaTotalPix()){
+			//Retorna a mola para a posição original (primeiro no modelo depois na imagem)
+			ma.getmP().getModeloMola().setTamanhoMolaAtualPix(ma.getmP().getModeloMola().getTamanhoMolaTotalPix());
+			vp.setImagemPropulsao(ma.getmP().getImagemPropulsao());
+			//Posiciona o Objeto na posição original
+			ma.getmO().setPosicaoXPx(130+reposicionaObjeto);
+			//Repaint
+			cOBAC.repinta();
+		}
+		//Inicia a movimentação do objeto
+		cIS.iniciarSimulacao();
+		//Para esta Thrad
+		t.interrupt();
+		t.stop();
+	}
+	
+}
--- trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/controle/propulsao/MolaDeslizanteListener.java
+++ trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/controle/propulsao/MolaDeslizanteListener.java
@@ -1,5 +1,6 @@
 package br.edu.ifg.formosa.obac.controle.propulsao;
 
+import java.awt.Color;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -11,27 +12,28 @@ import javax.swing.event.ChangeListener;
 import br.edu.ifg.formosa.obac.controle.obac.ControleOBAC;
 import br.edu.ifg.formosa.obac.controle.objetoAmbienteSuperficie.ControleInicioSimulacoes;
 import br.edu.ifg.formosa.obac.modelo.ModeloAmbiente;
+import br.edu.ifg.formosa.obac.utilidades.UtilidadeConvercoesEscala;
 import br.edu.ifg.formosa.obac.visao.VisaoPropulsao;
 
-public class MolaDeslisanteLitener implements ChangeListener, MouseListener{
+public class MolaDeslizanteListener implements ChangeListener, MouseListener{
 
 	//Referencias
 	private final VisaoPropulsao vP;
 	private final ModeloAmbiente mA;
 	private final ControleOBAC cOBAC;
 	private final ControleInicioSimulacoes cIS;
-	private ControleMolaSoltura cMS = null;
+	private ControleMolaSolturaDeslizante cMSD = null;
 	
 	//Variável de controle
 	private boolean mousePressionando = true; 
 	
-	public MolaDeslisanteLitener(VisaoPropulsao vP, ModeloAmbiente mA, ControleOBAC cOBAC, ControleInicioSimulacoes cIS) {
+	public MolaDeslizanteListener(VisaoPropulsao vP, ModeloAmbiente mA, ControleOBAC cOBAC, ControleInicioSimulacoes cIS) {
 		this.vP = vP;
 		this.mA = mA;
 		this.cOBAC = cOBAC;
 		this.cIS = cIS;
 		
-		this.vP.getDeslizanteMola().setValue(100);
+		this.vP.getDeslizanteMola().setValue(this.vP.getDeslizanteMola().getMaximum());
 		this.vP.getDeslizanteMola().addChangeListener(this);
 		this.vP.getDeslizanteMola().addMouseListener(this);
 	}
@@ -48,26 +50,38 @@ public class MolaDeslisanteLitener implements ChangeListener, MouseListener{
 	
 	private void comandos(){
 		if (mousePressionando==true) {
-			this.vP.getRotuloCompressao().setText("" +this.vP.getDeslizanteMola().getValue());
-			vP.setImagemPropulsao(
-					new ImageIcon(
-							mA.getmP().getImagemPropulsao().getImage().getScaledInstance(
-									this.vP.getDeslizanteMola().getValue(), 30, Image.SCALE_DEFAULT)));
+			
+			//Atualiza a imagem da mola com o tamnho adequado
+			vP.setImagemPropulsao(new ImageIcon(mA.getmP().getImagemPropulsao().getImage().getScaledInstance(this.vP.getDeslizanteMola().getValue(), 30, Image.SCALE_DEFAULT)));
+			//Move o objeto para que ele acompanhe os novos tamanhos da mola
 			mA.getmO().setPosicaoXPx(this.vP.getDeslizanteMola().getValue()+31);
+			//Seta o tamnho atual em pixels da mola
 			mA.getmP().getModeloMola().setTamanhoMolaAtualPix(this.vP.getDeslizanteMola().getValue());
+			//Seta o tamnho atual em metros da mola
+			mA.getmP().getModeloMola().setTamanhoMolaAtualM(
+					(UtilidadeConvercoesEscala.convertePixelMetro(//COnversão de Pixel para Metros
+							mA.getmP().getModeloMola().getTamanhoMolaTotalM(),//Tamanho total em M
+							mA.getmP().getModeloMola().getTamanhoMolaAtualPix(),//Tamanho atual em PX
+							mA.getmP().getModeloMola().getTamanhoMolaTotalPix())));//Tamanho Total Px
+
+			//Label que exibe o tamnho atual da mola
+			this.vP.getRotuloCompressao().setText(this.vP.getDeslizanteMola().getValue()+"%");
+			
+			this.vP.getDeslizanteMola().setBackground(Color.blue);
 		}
 		else{
+			this.vP.getDeslizanteMola().setBackground(Color.red);
 //			this.vP.getpCompressor().setVisible(false);
-//			cMS = new ControleMolaSoltura(vP, mA, cOBAC, cIS);
+			cMSD = new ControleMolaSolturaDeslizante(vP, mA, cOBAC, cIS);
 		}
 		this.cOBAC.repinta();
 		mousePressionando=true;
 	}
 	
 	private void testeLogico(){
-		if(this.vP.getDeslizanteMola().getValue()<100){
+		if(this.vP.getDeslizanteMola().getValue()<this.vP.getDeslizanteMola().getMaximum()){
 			this.mousePressionando=false;
-			this.vP.getDeslizanteMola().setValue(100);
+			this.vP.getDeslizanteMola().setValue(this.vP.getDeslizanteMola().getMaximum());
 			this.vP.getRotuloCompressao().setText("" +100);
 		}
 	}
