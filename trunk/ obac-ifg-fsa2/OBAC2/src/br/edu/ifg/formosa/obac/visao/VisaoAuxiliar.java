03ec5bf9b04fd2dd80c4b4f8f1f599804e6309a1
drakos.nevescunha8@gmail.com <drakos.nevescunha8@gmail.com@095db15f-677c-0e41-b5c9-00efc19637a4>
2014-09-24 18:29:11 +0000

    Passagem do slider da mola para a VisãoAuxiliar, de forma que ele não sofra as rotações e translações. Implementação do seu uso apenas quando a simulação for devidamente iniciada. Limpeza dos campos quando uma nova simulação é iniciada.

--- trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/visao/VisaoAuxiliar.java
+++ trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/visao/VisaoAuxiliar.java
@@ -1,9 +1,14 @@
 package br.edu.ifg.formosa.obac.visao;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.border.LineBorder;
 
 import br.edu.ifg.formosa.obac.modelo.ModeloAmbiente;
 
@@ -13,6 +18,10 @@ public class VisaoAuxiliar extends JPanel{
 	private ModeloAmbiente mA = null;
 	//--Visão
 	private VisaoPainelConfiguracao vPC = null;
+	//--Partes o Deslisante da mola
+		private JPanel pCompressor = null;
+		private JSlider deslizanteMola = null;
+		private JLabel rotuloCompressao = null;
 	
 	//Métodos
 	//--Construtor
@@ -23,10 +32,30 @@ public class VisaoAuxiliar extends JPanel{
 		
 		this.mA = mA;
 		this.vPC = vPC;
+		//Deslizante da mola
+			//Painel
+			pCompressor = new JPanel(null);
+				pCompressor.setBounds(30, 565, 690, 30);
+				pCompressor.setBackground(Color.white);
+				pCompressor.setBorder(new LineBorder(Color.black, 1));
+				pCompressor.setVisible(false);
+			this.add(pCompressor);
+			//Rotulo
+			rotuloCompressao = new JLabel("0%", JLabel.RIGHT);
+				rotuloCompressao.setSize(100, 30);
+				rotuloCompressao.setFont(new Font(null, 0, 20));
+				rotuloCompressao.setForeground(Color.black);
+			pCompressor.add(rotuloCompressao);
+			//Deslizante
+			deslizanteMola = new JSlider(JSlider.HORIZONTAL, 1, 100, 100);
+				deslizanteMola.setOpaque(false);
+				deslizanteMola.setBounds(100, 1, 589, 28);
+			pCompressor.add(deslizanteMola);
+		//Fim Deslizante da mola
 	}
 	
 	//--Paint
-	public void paint(Graphics g) {
+	public void paintComponent(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		
 		//Base do canhao - Só é desenhada aqui, pois ela não pode rotacionar, como rotaciona o VisaoPropulsao.
@@ -34,4 +63,7 @@ public class VisaoAuxiliar extends JPanel{
 			g2d.drawImage(mA.getmP().getImagemBaseCanhao().getImage(), mA.getmP().getPosXB(), mA.getmP().getPosYB(), this);
 		}
 	}
+	public JPanel getpCompressor() {return pCompressor;}
+	public JSlider getDeslizanteMola() {return deslizanteMola;}
+	public JLabel getRotuloCompressao() {return rotuloCompressao;}
 }
