bda416c823ad3f7a3c2775e690e04aad78d719e3
drakos.nevescunha8@gmail.com <drakos.nevescunha8@gmail.com@095db15f-677c-0e41-b5c9-00efc19637a4>
2015-03-11 22:06:14 +0000

    Ajuste no atrito do alumínio de 1.4 para 0.14

--- trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/modelo/ModeloSuperficie.java
+++ trunk/ obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/modelo/ModeloSuperficie.java
@@ -14,7 +14,7 @@ public class ModeloSuperficie {
 	//Constantes
 	//--Double
 	public static final double atritoMadeira = 0.62;
-	public static final double atritoAluminio = 1.4;
+	public static final double atritoAluminio = 0.14;
 	public static final double atritoAsfalto = 0.8;
 	
 	public static final double larguraSuperficie = 500; //Valor em px - OBAC1
