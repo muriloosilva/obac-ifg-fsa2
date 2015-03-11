ca73820f15b948f83fe1e6372e0b0dc8548fb157
drakos.nevescunha8@gmail.com <drakos.nevescunha8@gmail.com@095db15f-677c-0e41-b5c9-00efc19637a4>
2015-03-11 21:59:44 +0000

    Inserção da chamada ao método que calcula a velocidade pós impacto. Retirada de comentários desnecessários.

---  obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/controle/objetoAmbienteSuperficie/ControleObjeto5QuedaLivre.java
+++  obac-ifg-fsa2/OBAC2/src/br/edu/ifg/formosa/obac/controle/objetoAmbienteSuperficie/ControleObjeto5QuedaLivre.java
@@ -9,12 +9,10 @@ public class ControleObjeto5QuedaLivre implements ControleObjeto0Generico, Runna
 
 	//CONSTANTES
 	private final int atrasoMS = 20;//Atraso da thread usado no Sleep (20 milisegundos)
-	private final double atrasoSPadrao = 0.04;//Valor do tempo que ï¿½ incrementado a cada nova posiï¿½ï¿½o
-	private boolean continuar = true;///Variï¿½vel usada para pausar a simulaï¿½ï¿½o
+	private final double atrasoSPadrao = 0.04;//Valor do tempo que é incrementado a cada nova posição
+	private boolean continuar = true;///Variável usada para pausar a simulação
 	private boolean subida = false;
 	private boolean primeiraQueda = true;
-	//	private double soma = 0.0;
-	//	private double tempoLocal = 0.0;
 	double escala = 0.0;
 	double cacheAltura = 0;
 
@@ -22,7 +20,6 @@ public class ControleObjeto5QuedaLivre implements ControleObjeto0Generico, Runna
 	private VisaoPainelFormulas vpf;
 	private ControleOBAC cOBAC;
 	private ControleFormulasObjeto cfo;
-	private ControleFormulasSuperficie cfs;
 	//VARIï¿½VEL DA THREAD
 	private Thread t;
 
@@ -33,7 +30,6 @@ public class ControleObjeto5QuedaLivre implements ControleObjeto0Generico, Runna
 		this.vpf = vpf;
 		this.cOBAC = cOBAC;
 		this.cfo = cfo;
-		this.cfs = cfs;
 
 		cfo.calculaForcaNormal();
 		cfs.calculaForcaAtritoQueda();
@@ -48,25 +44,10 @@ public class ControleObjeto5QuedaLivre implements ControleObjeto0Generico, Runna
 //		escala = ma.getmEV().getEscalaFimYM()/ 470;
 		this.cacheAltura=ma.getmO().getPosicaoInicialYM();
 		ma.getmO().setPosFinalYMetros(cacheAltura*ma.getmO().getCoefRestituicao());
-
-//		System.out.println("Escala: "+escala);
-//		System.out.println("Coeficiente de Restituição: "+ma.getmO().getCoefRestituicao());
-//		System.out.println("Fim da escala Pixel: "+ma.getmEV().getEscalaFimYPix());
-		//		ma.getmO().setPosicaoYMetros(1000);Desnecessário isso
-		//		ma.getmO().setPosicaoInicialYM(1000);E isso também
 		
 		//Início da thread
 		t = new Thread(this);
 		t.start();
-
-//		System.out.println("Aceleração: " +ma.getmO().getAceleracao());
-
-
-		/*objectControl.calculaNormal(); --
-		surfaceControl.calculaAtrito(); --
-		objectControl.calculaAceleracaoFall(); --
-		surfaceControl.calculaEscalaFall(); --
-		objectControl.calculaCoefRestituicao(e); achei inutil rsrs*/
 		
 	}
 	double h0 = 1000;
@@ -74,65 +55,47 @@ public class ControleObjeto5QuedaLivre implements ControleObjeto0Generico, Runna
 	public void run() {
 		while(true){
 			if(continuar){				
-					//Aceleração>0 = Final da queda (bateu)
-				if(ma.getmO().getAceleracao()<0 && velocidadeZero()){
-					System.out.println("There|Inico da queda");
-					ma.getmO().setVelocidadeInicial(0);//V0=0
-					ma.getmO().setVelocidade(0);
-					acoesInversas();
-					ma.getmO().setPosicaoInicialYM(ma.getmO().getPosicaoYMetros());
-					h0=ma.getmO().getPosicaoYMetros();
-					if(ma.getmO().getPosicaoYMetros()<1.5)	break;
-				}
-				
+					//Aceleração>0 = Final da queda (bateu)				
 				if((ma.getmO().getAceleracao()>0 || !subida) && bateu()){// && ma.getmO().getPosicaoYMetros()<=this.cacheAltura){
 						System.out.println("Here|Inicio subida");
-//						double va=Math.sqrt(2*h0*ma.getGravSelecionada());
-						ma.getmO().setVelocidadeInicial(
-//								Math.sqrt(ma.getmO().getVelocidade())*(ma.getmO().getMassa()- 1000000000 *ma.getmO().getCoefRestituicao())/(ma.getmO().getMassa() + 1000000000)*(-1)
-								ma.getmO().getCoefRestituicao()*ma.getmO().getVelocidade()//va
-//								Math.sqrt(2*ma.getmO().getAceleracao()*h0)
-						);//V0=V*CoefR.
+						cfo.impactoQL();//V0=V*CoefR.
 						acoesInversas();
 						cacheAltura=ma.getmO().getPosicaoYMetros();
 						ma.getmO().setPosicaoInicialYM(0);
 						ma.getmO().setPosicaoYMetros(0);
 						
 						System.out.println("getVelocidadeInicial(): " +ma.getmO().getVelocidadeInicial());
-						//cfo.calculaNovaPosicaoY(0);//Subida
-						//ma.getmO().setPosicaoYMetros(ma.getmO().getPosicaoYMetros() * (-1));
 
 						if((int)ma.getmO().getVelocidade()<0)	break;
-
-						//Calcula a altura máxima da próxima
-//						ma.getmO().setPosFinalYMetros(cacheAltura*ma.getmO().getCoefRestituicao());
 					}
 					//Aceleração<0 = Final da Subida (caiu de novo)
-					
-					if(bateu() && velocidadeZero()){break;}
+					if(ma.getmO().getAceleracao()<0 && velocidadeZero()){
+						System.out.println("There|Inico da queda");
+						ma.getmO().setVelocidadeInicial(0);//V0=0
+						ma.getmO().setVelocidade(0);
+						acoesInversas();
+						ma.getmO().setPosicaoInicialYM(ma.getmO().getPosicaoYMetros());
+						h0=ma.getmO().getPosicaoYMetros();
+						
+						//Condição para parada, evitando loop infinito
+						if(ma.getmO().getPosicaoYMetros()<1.5)	break;
+					}
 				
-					//Systens
-					System.out.println("getPosicaoYMetros(): " +ma.getmO().getPosicaoYMetros());
-					System.out.println("getVelocidade(): " +ma.getmO().getVelocidade());
-					System.out.println("getAceleracao(): " +ma.getmO().getAceleracao());
-					System.out.println("________________________________________");
+					if(bateu() && velocidadeZero()){break;}
 
 					//Movimentação do objeto
 					ma.setTempoAtual(ma.getTempoAtual() + atrasoSPadrao);
 					ma.getmO().setVelocidade(ma.getmO().getVelocidadeInicial()+ma.getmO().getAceleracao()*ma.getTempoAtual());
 
 					cfo.calculaNovaPosicaoY();
-//					cfo.calculaVelocidadeTorricelli(ma.getmO().getPosicaoYMetros());
 					if(!subida && primeiraQueda)
 						ma.getmO().setPosicaoYMetros(1000 - ma.getmO().getPosicaoYMetros());
-					//else if(!subida && !primeiraQueda && ma.getmO().getPosicaoInicialYM()>=1000)
-					//		ma.getmO().setPosicaoYMetros((1000+(1000-ma.getmO().getPosicaoYMetros())));
-					else if(!subida && !primeiraQueda) //&& ma.getmO().getPosicaoInicialYM()<1000)
+					else if(!subida && !primeiraQueda)
 						ma.getmO().setPosicaoYMetros( ma.getmO().getPosicaoInicialYM()+(ma.getmO().getPosicaoInicialYM()-ma.getmO().getPosicaoYMetros()));
 
 					//Parada no carregamento para dar o realismo da simulação
 					//Esta ocorre no final para possibilitar a pausa da simulação
-					try {	t.sleep(atrasoMS);	}
+					try {	Thread.sleep(atrasoMS);	}
 					catch (InterruptedException e) {System.err.println("Erro na Thread!");}
 				}
 			
@@ -141,7 +104,6 @@ public class ControleObjeto5QuedaLivre implements ControleObjeto0Generico, Runna
 			//__> Aqui está subitraindo 30px pois os mesmos foram retirados da escala
 			//____Por hora corrigiu o impacto com o solo, mas a parte da decida continua falha
 			//____COm o coeficiente em 1 a simulação funciona bem, exceto por um glitch no momento da queda
-
 
 
 			cOBAC.repinta();
@@ -158,9 +120,8 @@ public class ControleObjeto5QuedaLivre implements ControleObjeto0Generico, Runna
 	//Métodos privados
 	//---Verifica o momento do impacto do objeto
 	private boolean bateu(){
-		if(ma.getmO().getPosicaoYPx()>(UtilidadeConvercoesEscala.metroParaPixelV(ma.getmEV(), 0) - 30)
-			|| (int)ma.getmO().getPosicaoYMetros()<0
-			){
+		if(ma.getmO().getPosicaoYPx()>(UtilidadeConvercoesEscala.metroParaPixelV(ma.getmEV(), 0) - 30)|| (int)ma.getmO().getPosicaoYMetros()<0)
+		{
 			System.out.println("Hitted|Bateu");
 			return true;
 		}
@@ -185,9 +146,7 @@ public class ControleObjeto5QuedaLivre implements ControleObjeto0Generico, Runna
 		ma.setTempoTotal(ma.getTempoTotal()+ma.getTempoAtual());//TempoT+=TempoA
 		ma.setTempoAtual(0);//TempoA=0
 		subida=!subida;
-		
-//		ma.getmO().setPosicaoYMetros(ma.getmO().getPosicaoYMetros());
-				this.primeiraQueda=false;
+		this.primeiraQueda=false;
 	}
 
 	//Continuar
